a29ecbe417addc3b350b2a273170e53cf3757468
Dani Demas <mediasand@wl-217-62.wpa.claremont.edu>
2014-09-10 14:06:22 -0700

    Added big, black rectangle and “Hit me!” button

---
+++ viewTutorial/DDGrid.m
@@ -0,0 +1,31 @@
+//
+//  DDGrid.m
+//  viewTutorial
+//
+//  Created by Dani Demas on 9/9/14.
+//  Copyright (c) 2014 Dani Demas. All rights reserved.
+//
+
+#import "DDGrid.h"
+
+@implementation DDGrid
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
