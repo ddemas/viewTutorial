f81b9acb272540cd889ac42ca0d3db8f39530f28
Dani Demas <mediasand@wl-217-62.wpa.claremont.edu>
2014-09-09 22:50:39 -0700

    Initial Commit

---
+++ viewTutorial/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  viewTutorial
+//
+//  Created by Dani Demas on 9/9/14.
+//  Copyright (c) 2014 Dani Demas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ viewTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  viewTutorial
+//
+//  Created by Dani Demas on 9/9/14.
+//  Copyright (c) 2014 Dani Demas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
